439dadbf94852536d947225cf71693ba8f0e3be2
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-06-22 04:08:46 +1200

    add GET /question/response dummy endpint

--- backend/src/routes/QuestionRouter.js
+++ backend/src/routes/QuestionRouter.js
@@ -42,6 +42,73 @@ const controller = new QuestionController()
 */
 router.get('/experience', controller.GetQuestionsWithExperiences)
 
+/**
+ * @openapi
+ * /question/response:
+ *  get:
+ *      description: Get all the questions each with the user's relevant experiences and saved responses for that question
+ *      responses:
+ *          200:
+ *              description: A list of questions, each with a list of relevant expriences and saved responses
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              type: object
+ *                              required:
+ *                                  - _id
+ *                                  - questionText
+ *                              properties:
+ *                                  _id:
+ *                                      type: string
+ *                                  questionText:
+ *                                      type: string
+ *                                      example: Tell me about a time when you experienced a conflict
+ *                                  experiences:
+ *                                      type: array
+ *                                      items:
+ *                                          type: object
+ *                                          required:
+ *                                              - _id
+ *                                              - value
+ *                                          properties:
+ *                                              _id:
+ *                                                  type: string
+ *                                              value:
+ *                                                  type: string
+ *                                  responses:
+ *                                      type: array
+ *                                      items:
+ *                                          type: object
+ *                                          required:
+ *                                              - experience
+ *                                              - answer
+ *                                          properties:
+ *                                              experience:
+ *                                                  type: object
+ *                                                  required:
+ *                                                      - _id
+ *                                                      - value
+ *                                              answer:
+ *                                                  type: object
+ *                                                  required:
+ *                                                      - s
+ *                                                      - t
+ *                                                      - a
+ *                                                      - r
+ *                                                  properties:
+ *                                                      s:
+ *                                                          type: string
+ *                                                      t:
+ *                                                          type: string
+ *                                                      a:
+ *                                                          type: string
+ *                                                      r:
+ *                                                          type: string
+*/
+router.get('/response', controller.GetQuestionsWithResponses)
+
 export default router
 
 
