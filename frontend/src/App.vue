aac0ad921111a32a5c70c4ed890f081485b22877
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-07-06 08:18:39 +1200

    add navbar component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/lecture">Lecture</router-link>
-  </nav>
-
-  <QuizOption />
-
-  <router-view />
-  <button>click</button>
+  <div class="app">
+    <NavBar />
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/lecture">Lecture</router-link>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
 <script>
 import "./assets/css/styles.scss";
+import NavBar from "./components/NavBar.vue";
 
 export default {
   name: "App",
-  components: {},
+  components: {
+    NavBar,
+  },
 };
 </script>
 
-<style lang="scss"></style>
+<style scoped lang="scss">
+.app {
+  display: flex;
+  flex-direction: row;
+
+  background: lightblue;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="navBar">hello</div>
+</template>
+
+<style scoped>
+.navBar {
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NavBar",
+  components: {},
+});
+</script>
