ce2fea77196df2d2092cd48e71122f91c7c88068
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-07-06 10:18:06 +1200

    add section title component

---
+++ frontend/src/components/SectionTitle.vue
@@ -0,0 +1,36 @@
+<template>
+  <h2>{{ title }}</h2>
+  <div class="underline"></div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/css/theme.scss";
+
+h2 {
+  font-size: 20px;
+  font-weight: 400;
+
+  margin-bottom: 5px;
+  margin-top: 40px;
+}
+
+.underline {
+  width: 70px;
+  height: 3px;
+
+  background-color: $c-primary;
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "SectionTitle",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
