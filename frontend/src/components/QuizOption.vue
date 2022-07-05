bcd6ea7b9f9d18028dcb01fa730a53023db85f4e
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-07-06 11:04:33 +1200

    finish styling exampe quote

---
+++ frontend/src/components/QuizOption.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="container"></div>
+</template>
+<script>
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  name: "QuizOption",
+  props: {
+    optionNumber: Number,
+    description: String,
+  },
+});
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+}
+</style>
