4d86f43f32008b0ba91bce37a35992ba2e2698a5
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-07-06 10:18:29 +1200

    add section paragraph component

---
+++ frontend/src/components/SectionParagraph.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="section-paragraph">
+    <SectionTitle title="Section title here" />
+
+    <p>
+      <slot />
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import SectionTitle from "./SectionTitle.vue";
+export default defineComponent({
+  name: "SectionParagraph",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  components: {
+    SectionTitle,
+  },
+});
+</script>
--- frontend/src/views/LectureView.vue
+++ frontend/src/views/LectureView.vue
@@ -14,14 +14,36 @@
       sirloin shoulder boudin pig. Ribeye shoulder spare ribs pig. Filet mignon
       kielbasa pig, frankfurter swine meatball tri-tip. Tri-tip meatball shank
       filet mignon, burgdoggen pork chop chislic pastrami porchetta ball tip
-      sirloin shankle pancetta venison spare ribs. <br />Ball tip tri-tip tongue
-      turkey, ham fatback tenderloin pastrami jerky sausage brisket beef
-      bresaola pig frankfurter. Pork frankfurter jerky shoulder leberkas
-      hamburger. Bresaola ribeye tail kielbasa corned beef pastrami shankle cow
-      chuck pancetta flank tenderloin swine. Andouille venison porchetta, jowl
-      chislic pork loin filet mignon short ribs rump sausage kielbasa ground
-      round alcatra shank.
+      sirloin shankle pancetta venison spare ribs.
+      <br />
+      <br />
+      Ball tip tri-tip tongue turkey, ham fatback tenderloin pastrami jerky
+      sausage brisket beef bresaola pig frankfurter. Pork frankfurter jerky
+      shoulder leberkas hamburger. Bresaola ribeye tail kielbasa corned beef
+      pastrami shankle cow chuck pancetta flank tenderloin swine. Andouille
+      venison porchetta, jowl chislic pork loin filet mignon short ribs rump
+      sausage kielbasa ground round alcatra shank.
     </TitleBlock>
+
+    <SectionParagraph title="Building your interview">
+      Pig fatback jerky shankle sausage. Porchetta spare ribs turducken, tail
+      salami cupim flank pork loin pig meatloaf brisket turkey ham hock swine
+      strip steak. Sirloin chicken ground round bacon, kielbasa chuck kevin
+      short ribs. Short loin chuck salami pork chop fatback. Pork loin short
+      loin pastrami short ribs frankfurter salami strip steak brisket leberkas
+      sirloin shoulder boudin pig.
+      <br />
+      <br />
+      Ribeye shoulder spare ribs pig. Filet mignon kielbasa pig, frankfurter
+      swine meatball tri-tip. Tri-tip meatball shank filet mignon, burgdoggen
+      pork chop chislic pastrami porchetta ball tip sirloin shankle pancetta
+      venison spare ribs. Ball tip tri-tip tongue turkey, ham fatback tenderloin
+      pastrami jerky sausage brisket beef bresaola pig frankfurter. Pork
+      frankfurter jerky shoulder leberkas hamburger. Bresaola ribeye tail
+      kielbasa corned beef pastrami shankle cow chuck pancetta flank tenderloin
+      swine. Andouille venison porchetta, jowl chislic pork loin filet mignon
+      short ribs rump sausage kielbasa ground round alcatra shank.
+    </SectionParagraph>
   </div>
 </template>
 
@@ -36,11 +58,12 @@
 </style>
 
 <script lang="ts">
+import SectionParagraph from "@/components/SectionParagraph.vue";
 import TitleBlock from "@/components/TitleBlock.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "LectureView",
-  components: { TitleBlock },
+  components: { TitleBlock, SectionParagraph },
 });
 </script>
