6c00e7178f9d409fd58a95af32eb74c547308782
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-07-06 13:26:26 +1200

    implement module items on yay you did it page

---
+++ frontend/src/components/ModuleItem.vue
@@ -0,0 +1,81 @@
+<template>
+  <div
+    class="module-item"
+    :class="{
+      past: moduleType === 'past',
+      current: moduleType === 'current',
+      next: moduleType === 'next',
+      future: moduleType === 'future',
+    }"
+  >
+    <p>hello</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/css/theme.scss";
+
+.module-item {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  box-sizing: border-box;
+  padding: 0 20px;
+
+  height: 40px;
+  width: 500px;
+  border-radius: 6px;
+
+  p {
+    font-size: 14px;
+    margin: 0;
+  }
+
+  &.past {
+    border: solid 1px $c-grey-light;
+    background: $c-bg-grey;
+
+    p {
+      color: $c-grey-dark;
+      text-decoration: line-through;
+    }
+  }
+
+  &.current {
+    border: solid 1px $c-primary;
+    background: rgba($c-primary, 0.2);
+
+    p {
+      color: $c-primary;
+      text-decoration: line-through;
+    }
+  }
+
+  &.next {
+    border: solid 1px $c-grey-light;
+
+    p {
+      color: $c-black;
+    }
+  }
+}
+</style>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  name: "ModuleItem",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    moduleType: {
+      type: String as PropType<"past" | "current" | "next" | "future">,
+      required: true,
+    },
+  },
+});
+</script>
---
+++ frontend/src/views/ModuleEndView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="module-end-view">
+    <div class="header">
+      <h1>Yay! You did it! 🎉</h1>
+      <h3>Finished module: <span>Module name</span></h3>
+    </div>
+    <div class="content">
+      <ModuleItem name="Finding the right examples" moduleType="past" />
+      <ModuleItem name="Listening for the correct cues" moduleType="current" />
+      <ModuleItem name="Structuring responses" moduleType="next" />
+      <ModuleItem name="Bacon ipsum dolor amet" moduleType="future" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/css/theme.scss";
+
+.module-end-view {
+  padding: 20px 40px;
+
+  overflow-x: scroll;
+
+  h1 {
+    font-size: 36px;
+    font-weight: 400;
+
+    margin-bottom: 15px;
+  }
+
+  h1 + h3 {
+    font-size: 16px;
+    font-weight: 400;
+
+    color: $c-grey-dark;
+
+    margin: 0;
+
+    > span {
+      color: $c-secondary;
+    }
+  }
+}
+</style>
+
+<script lang="ts">
+import ModuleItem from "@/components/ModuleItem.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ModuleEndView",
+  components: { ModuleItem },
+});
+</script>
