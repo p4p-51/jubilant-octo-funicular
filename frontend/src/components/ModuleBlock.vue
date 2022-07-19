307ae7c8d8bcd84c23aaa2ddda4741fc7e507c50
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-07-19 12:11:04 +1200

    create module block component for planning page

---
+++ frontend/src/components/ModuleBlock.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="module-block">
+    <div class="number">{{ number }}</div>
+    <div class="description">
+      <h5>{{ title }}</h5>
+      <p>
+        {{ description }}
+      </p>
+    </div>
+    <img :src="image" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/css/theme.scss";
+</style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "ModuleBlock",
+  props: {
+    number: {
+      type: Number,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  components: {},
+});
+</script>
