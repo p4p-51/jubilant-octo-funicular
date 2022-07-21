bcae3c768783f9133c87b7ab6bf11fab09fd7608
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-07-21 21:39:57 +1200

    style experiences

--- frontend/src/components/AddExperiences.vue
+++ frontend/src/components/AddExperiences.vue
@@ -2,8 +2,17 @@
   <div class="add-experiences">
     <h2>List down some of your past teamwork experiences</h2>
     <div class="content">
-      <div class="create-experiences"></div>
-      <div class="checklist">checklikst</div>
+      <div class="create-experiences">
+        <button class="box">+ Add a new experience</button>
+        <div>somethign</div>
+      </div>
+      <div class="checklist">
+        <h3>Your experiences</h3>
+        <p v-for="c in checklist" :key="c.label">
+          <span class="tick" v-if="c.isFilled">✓ </span>
+          <span class="dot" v-else>• </span>{{ c.label }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -13,7 +22,6 @@
 
 .add-experiences {
   width: 100%;
-  background: lightblue;
 
   > h2 {
     font-size: 20px;
@@ -26,6 +34,61 @@
   .content {
     display: flex;
     justify-content: space-between;
+    column-gap: 70px;
+
+    .add-experiences {
+      flex: 1;
+    }
+
+    .create-experiences {
+      flex: 2;
+      display: flex;
+
+      flex-direction: column;
+
+      margin-top: 30px;
+
+      .box {
+        border: solid 1px $c-grey-light;
+        border-radius: 0;
+
+        max-width: 1000px;
+
+        padding: 20px 25px;
+
+        &:hover {
+          background: rgba($c-primary, 0.1);
+          border: solid 1px $c-primary;
+          transform: none;
+        }
+      }
+    }
+    .checklist {
+      width: 30%;
+      box-sizing: border-box;
+
+      h3 {
+        font-size: 20px;
+        font-weight: 400;
+      }
+
+      p {
+        margin-left: 20px;
+
+        .dot {
+          color: orange;
+        }
+
+        .tick {
+          color: green;
+        }
+      }
+
+      p > span {
+        font-weight: bold;
+        margin-right: 10px;
+      }
+    }
   }
 }
 </style>
@@ -37,7 +100,34 @@ export default defineComponent({
   name: "AddExperiences",
   components: {},
   data() {
-    return {};
+    return {
+      checklist: [
+        {
+          label: "Conflict",
+          isFilled: false,
+        },
+        {
+          label: "Leadership",
+          isFilled: false,
+        },
+        {
+          label: "Communication",
+          isFilled: false,
+        },
+        {
+          label: "Teamwork",
+          isFilled: true,
+        },
+        {
+          label: "Collaboration",
+          isFilled: false,
+        },
+        {
+          label: "Failure",
+          isFilled: false,
+        },
+      ],
+    };
   },
   props: {},
 });
