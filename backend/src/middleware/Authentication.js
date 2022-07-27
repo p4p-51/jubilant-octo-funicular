99e4e437eb2bb5223ee414581ff4c67e0011e6f6
Daniel Li <danielli12357@gmail.com>
2022-07-28 05:25:25 +1200

    disable firebase for now

--- backend/src/middleware/Authentication.js
+++ backend/src/middleware/Authentication.js
@@ -2,15 +2,16 @@ import { getAuth } from 'firebase-admin/auth';
 
 export async function isAuthenticated(req, res, next) {
   // Extract the token from "Authorization: Bearer <Token>"
-  const authToken = req.headers['authorization'].slice(7);
-  try {
-    await getAuth().verifyIdToken(authToken).then((decodedToken) => {
-      res.locals = { ...res.locals, uid: decodedToken.uid, role: decodedToken.role, email: decodedToken.email };
-      console.log('decodedToken', JSON.stringify(decodedToken));
-      return next()
-    });
-  } catch (err) {
-    console.error(`${err.code} -  ${err.message}`);
-    return res.status(401).send({ code: "401", message: 'Unauthorized' });
-  }
+  // const authToken = req.headers['authorization'].slice(7);
+  // try {
+  //   await getAuth().verifyIdToken(authToken).then((decodedToken) => {
+  //     res.locals = { ...res.locals, uid: decodedToken.uid, role: decodedToken.role, email: decodedToken.email };
+  //     console.log('decodedToken', JSON.stringify(decodedToken));
+  //     return next()
+  //   });
+  // } catch (err) {
+  //   console.error(`${err.code} -  ${err.message}`);
+  //   return res.status(401).send({ code: "401", message: 'Unauthorized' });
+  // }
+  next()
 }
