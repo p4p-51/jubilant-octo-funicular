e64fd677c2916402dc2d29e45afdfc682ab3cba0
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-08-11 01:39:05 +1200

    commit landing

--- frontend/src/views/LandingView.vue
+++ frontend/src/views/LandingView.vue
@@ -31,16 +31,59 @@
     </div>
 
     <div class="main-content feature-block">
-      <h3>Title</h3>
-      <p>smetiing</p>
+      <div class="text">
+        <h3>Pork loin short loin pastrami short ribs frankfurter</h3>
+        <p>
+          Ribeye shoulder spare ribs pig. Filet mignon kielbasa pig, frankfurter
+          swine meatball tri-tip. Tri-tip meatball shank filet mignon,
+          burgdoggen pork chop chislic pastrami porchetta ball tip sirloin
+          shankle pancetta venison spare ribs. Ball tip tri-tip tongue turkey,
+          ham fatback tenderloin pastrami jerky sausage brisket beef bresaola
+          pig frankfurter. Pork frankfurter jerky shoulder leberkas hamburger.
+          Bresaola ribeye tail kielbasa corned beef pastrami shankle cow chuck
+          pancetta flank tenderloin swine. Andouille venison porchetta, jowl
+          chislic pork loin filet mignon short ribs rump sausage kielbasa ground
+          round alcatra shank.
+        </p>
+      </div>
+      <img src="@/assets/illustrations/person-money.svg" />
+    </div>
+    <div class="main-content feature-block reverse">
+      <div class="text">
+        <h3>Ribeye shoulder spare ribs pig</h3>
+        <p>
+          Andouille chislic turkey doner, beef ribs ground round porchetta
+          t-bone pancetta drumstick rump kielbasa. Doner cupim flank tenderloin
+          short loin bacon shankle. Prosciutto drumstick leberkas flank,
+          frankfurter corned beef shoulder sausage filet mignon beef ribs rump
+          pastrami. Drumstick brisket turkey t-bone picanha spare ribs short
+          ribs hamburger cupim pork chop burgdoggen shank. Kevin sirloin
+          frankfurter salami ball tip alcatra short ribs, jerky tri-tip pork
+          loin prosciutto meatball. Turducken kevin jerky ball tip burgdoggen
+          tail cupim spare ribs. T-bone doner shank cupim.
+        </p>
+      </div>
+      <img src="@/assets/illustrations/content-people.svg" />
     </div>
     <div class="main-content feature-block">
-      <h3>Title</h3>
-      <p>smetiing</p>
-    </div>
-    <div class="main-content feature-block">
-      <h3>Title</h3>
-      <p>smetiing</p>
+      <div class="text">
+        <h3>Tail brisket shoulder shankle chicken buffalo</h3>
+        <p>
+          Pig fatback jerky shankle sausage. Porchetta spare ribs turducken,
+          tail salami cupim flank pork loin pig meatloaf brisket turkey ham hock
+          swine strip steak. Sirloin chicken ground round bacon, kielbasa chuck
+          kevin short ribs. Short loin chuck salami pork chop fatback. Pork loin
+          short loin pastrami short ribs frankfurter salami strip steak brisket
+          leberkas sirloin shoulder boudin pig. Pig fatback jerky shankle
+          sausage. Porchetta spare ribs turducken, tail salami cupim flank pork
+          loin pig meatloaf brisket turkey ham hock swine strip steak. Sirloin
+          chicken ground round bacon, kielbasa chuck kevin short ribs. Short
+          loin chuck salami pork chop fatback. Pork loin short loin pastrami
+          short ribs frankfurter salami strip steak brisket leberkas sirloin
+          shoulder boudin pig.
+        </p>
+      </div>
+      <img src="@/assets/illustrations/girl-balloons.svg" />
     </div>
     <footer>footer</footer>
   </div>
@@ -58,8 +101,6 @@
   flex-direction: column;
 
   header {
-    background: lightblue;
-
     padding: 45px 80px;
 
     display: flex;
@@ -105,7 +146,7 @@
   }
 
   .main-content {
-    padding: 100px 200px;
+    margin: 100px 200px;
   }
 
   .landing-content {
@@ -113,7 +154,7 @@
     align-items: center;
     justify-content: space-between;
 
-    padding-top: 0;
+    margin-top: 0;
 
     .text-container {
       display: flex;
@@ -178,6 +219,42 @@
 
     > img {
       width: 800px;
+    }
+  }
+
+  .features-list {
+    background: $c-bg-grey;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    height: 240px;
+  }
+
+  .feature-block {
+    $image-margin: 100px;
+
+    display: flex;
+    align-items: center;
+
+    > img {
+      width: 500px;
+      margin-left: $image-margin;
+    }
+
+    > div {
+      display: flex;
+      flex-direction: column;
+      row-gap: 50px;
+    }
+
+    &.reverse {
+      flex-direction: row-reverse;
+
+      > img {
+        margin-right: $image-margin;
+      }
     }
   }
 }
