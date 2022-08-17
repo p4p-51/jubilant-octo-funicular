168b172506f35007855d3cecb5e02fd0aa3aed85
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-08-17 16:14:27 +1200

    update z index of modals

--- frontend/src/components/popups/ConfirmationPopup.vue
+++ frontend/src/components/popups/ConfirmationPopup.vue
@@ -48,7 +48,7 @@ export default defineComponent({
   width: 100%;
   height: 100%;
   background: rgba(0, 0, 0, 0.5);
-  z-index: 9999;
+  z-index: 9990;
   display: flex;
   justify-content: center;
   align-items: center;
