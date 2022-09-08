14ae1d696f761b3eefc6d3ea9194bd3b0600fe1f
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-09-08 15:49:58 +1200

    try something

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -126,7 +126,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 });
 
