91c463cc6fdb81e07804259309a0e8a9e9b12f0e
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-09-08 22:23:09 +1200

    revert router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,5 @@
 /* tslint:disable */
-import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import { RoutesManager } from "./routes";
 
@@ -126,7 +126,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
