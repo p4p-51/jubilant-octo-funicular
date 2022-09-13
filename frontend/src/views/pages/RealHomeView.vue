bfe226bdc3f281072bfcff631acc56dcaecdb69d
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-09-14 00:29:03 +1200

    set up media query for illutro container

--- frontend/src/views/pages/RealHomeView.vue
+++ frontend/src/views/pages/RealHomeView.vue
@@ -9,7 +9,7 @@
         <button>Let's get started or something</button>
       </div>
     </div>
-    <div class="illustratrion-container">
+    <div class="illustration-container">
       <img src="@/assets/illustrations/girl-studying.svg" />
     </div>
   </div>
@@ -21,11 +21,29 @@
 .real-home-view {
   display: flex;
 
+  height: 100%;
+  width: 100%;
+
   .content {
     width: 55%;
-    min-width: 400px;
+    min-width: 600px;
   }
 
-  
+  .illustration-container {
+    background-color: rgba($c-primary, 0.2);
+
+    display: flex;
+    justify-content: center;
+
+    img {
+      width: 80%;
+    }
+  }
+}
+
+@media (max-width: 1080px) {
+  .illustration-container {
+    visibility: hidden;
+  }
 }
 </style>
