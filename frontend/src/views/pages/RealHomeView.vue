e02c592e6a5196b1bc4081ab544ee9c2010ea47b
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-09-14 00:20:49 +1200

    add new file for real home page

---
+++ frontend/src/views/pages/RealHomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="real-home-view">
+    <div class="content">
+      <h1>Hi, name</h1>
+      <h2>Ready to take your behavioural skills to the next level?</h2>
+
+      <div class="continue-prompt">
+        <p>Current module: <span>Welcome</span></p>
+        <button>Let's get started or something</button>
+      </div>
+    </div>
+    <div class="illustratrion-container">
+      <img src="@/assets/illustrations/girl-studying.svg" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/css/theme.scss";
+
+.real-home-view {
+  display: flex;
+
+  .content {
+    width: 55%;
+    min-width: 400px;
+  }
+
+  
+}
+</style>
